888f81900d3dab360b7dc00512a1cb962bbaeb57
Roger Crescêncio <rogercrescencio@MacBook-Pro-de-Roger.local>
2024-03-27 11:24:29 -0300

    feat: Publish package configs formatting code

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  extends: ['universe/native'],
+  globals: {
+    __dirname: true,
+  },
+  rules: {
+    'max-lines': [
+      'warn',
+      { max: 120, skipBlankLines: true, skipComments: true },
+    ],
+
+    // allow .js files to contain JSX code
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+
+    // ignore errors for the react-navigation package
+    'react/prop-types': [
+      'error',
+      { ignore: ['navigation', 'navigation.navigate'] },
+    ],
+  },
+};
