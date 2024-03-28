17d32458b13e96527ca712543d3ca41dc9faeb5d
Roger Crescêncio <rogercrescencio@MacBook-Pro-de-Roger.local>
2024-03-28 14:30:03 -0300

    refactor: refactored installation of dependencies

--- eslintrc.js
+++ eslintrc.js
@@ -1,10 +1,14 @@
 module.exports = {
   root: true,
+  plugins: ['unused-imports', 'import'],
   extends: ['universe/node', 'universe/web', 'universe/native'],
   globals: {
     __dirname: true,
   },
   rules: {
+    'react/prop-types': 'off',
+    'import/no-unused-modules': 'error',
+    'unused-imports/no-unused-imports': 'error',
     'max-lines': [
       'warn',
       { max: 120, skipBlankLines: true, skipComments: true },
@@ -16,7 +20,7 @@ module.exports = {
     // ignore errors for the react-navigation package
     'react/prop-types': [
       'error',
-      { ignore: ['navigation', 'navigation.navigate'] },
+      { ignore: ['navigation', 'navigation.navigate', 'Component','children','pageProps'] },
     ],
   },
 };
